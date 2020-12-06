fc3d16b7f609ec9b3cb510c52cff9f5f54dec207
Rachel <rachel.rolon@gmail.com>
2020-12-05 21:19:16 -0600

    completed divide by zero exception

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -8,17 +8,41 @@ namespace csharp_web_dev_lsn9exceptions
         static double Divide(double x, double y)
         {
             // Write your code here!
+            if (y.Equals(0))
+            {
+                throw new ArgumentOutOfRangeException("Cannot divide by zero. Check your input and try again.");
+            }
+
+            return x / y;
         }
 
-        static int CheckFileExtension(string fileName)
-        {
+        //static int CheckFileExtension(string fileName)
+        //{
             // Write your code here!
-        }
+            //If a student’s submitted file ends in .cs, they get 1 point.If a student’s 
+            //    submitted file doesn’t end in .cs, they get 0 points.If the file submitted 
+            //    is null or an empty string, an exception should be thrown. What kind of exception is up to you!
+            //return integer representing the number of points a student receives for properly submitting a file in C# 
+        //}
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try 
+            {
+                Console.WriteLine("Enter student point score:");
+                int points = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter total possible points:");
+                int total = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Student grade: {Divide(points, total)}");
+
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("cannot divide by zero.  Check your input and try again");
+            }
+
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -28,6 +52,6 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Stefanie", "CoolProgram.cs");
 
 
-        }
+            }
     }
 }
